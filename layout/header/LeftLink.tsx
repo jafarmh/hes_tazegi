e25b088e4cee2092938cdf58f953883a26da2490
jafarmh <jafar.m@yahoo.com>
2024-04-07 09:02:21 +0330

    header action

--- layout/header/LeftLink.tsx
+++ layout/header/LeftLink.tsx
@@ -5,13 +5,18 @@ import React, { useState } from 'react'
 import HorizontalRed from '@/Components/html/HorizontalRed';
 import down from '@/asset/icon/down.svg'
 
-export const LeftLink = ({setShowCategory}:{setShowCategory:Function}) => {
-    const [rotate,setRotate]=useState(false);
+export const LeftLink = ({ setShowCategory }: { setShowCategory: Function }) => {
+    const [rotate, setRotate] = useState(false);
+    const [pagesShow, setPagesShow] = useState(false);
 
-    const ClickCategory=()=>{
+    const ClickCategory = () => {
         setRotate(!rotate);
         setShowCategory()
     }
+
+
+ 
+    
 
     return (
         <>
@@ -21,20 +26,31 @@ export const LeftLink = ({setShowCategory}:{setShowCategory:Function}) => {
                         MEGA.news
                     </b>
                 </div>
-                <div onClick={ClickCategory }  className='flex flex-row gap-1 items-center '>
+                <div onClick={ClickCategory} className='flex flex-row gap-1 items-center '>
                     <div className=' flex flex-col gap-1'>
                         <b>Categories</b>
-                        <span>  {rotate&& <HorizontalRed/>}</span>
-                        
+                        <span>  {rotate && <HorizontalRed />}</span>
+
                     </div>
-                    <img style={rotate?{ transform:'rotate(-180deg)', transitionDuration:"1s" }:{ transform:'rotate(0deg)', transitionDuration:"1s"}} src={down.src} alt="down" />
+                    <img style={rotate ? { transform: 'rotate(-180deg)', transitionDuration: "1s" } : { transform: 'rotate(0deg)', transitionDuration: "1s" }} src={down.src} alt="down" />
                 </div>
-              
-                <div className='flex flex-row gap-1 items-center'>
-                    <b >
-                        Pages
-                    </b>
-                    <img src={down.src} alt="down" />
+
+                <div onMouseOver={()=>setPagesShow(true)} onMouseOut={()=>setPagesShow(false)} className='flex flex-row gap-1 items-center relative'>
+
+                    <div className=' flex flex-col gap-1'>
+                        <b>Pages</b>
+                        <span>  {pagesShow && <HorizontalRed />}</span>
+
+                    </div>
+                    <img style={pagesShow ? { transform: 'rotate(-180deg)', transitionDuration: "1s" } : { transform: 'rotate(0deg)', transitionDuration: "1s" }} src={down.src} alt="down" />
+
+                    {pagesShow && <div className='absolute top-10 bg-white rounded-[--radius] w-[200px]  z-20 shadow-[--shadow] '>
+                        <div className='flex flex-col gap-3  p-8'>
+
+                            {["One", "Two", "Three", "Four"].map((item) => <b>Sub menu {item}</b>)}
+                        </div>
+
+                    </div>}
                 </div>
                 <div>
                     <b  >
@@ -46,7 +62,7 @@ export const LeftLink = ({setShowCategory}:{setShowCategory:Function}) => {
                         About us
                     </b>
                 </div>
-            
+
             </div>
         </>
     )
